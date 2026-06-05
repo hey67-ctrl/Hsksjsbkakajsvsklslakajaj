48d087a0cc092565158802f0eab70722b092e014
hey67-ctrl <safeshellez6@gmail.com>
2026-06-05 21:48:08 +0800

    Update console log message format in server.js

--- kahoot-bot/server.js
+++ kahoot-bot/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080; // FORCE PORT 8080
+const PORT = process.env.PORT || 8080; // ONLY USE 8080
 
 app.use(express.static(__dirname));
 
 app.listen(PORT, () => {
-  console.log('✅ Running on port', PORT);
+  console.log("Running on port", PORT);
 });
